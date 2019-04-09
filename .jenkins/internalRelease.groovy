1c5232a5f9eaf1509df7c3c3713e2b1d29852d8a
obogdanov <oleksandr.bogdanov@grandcentrix.net>
2019-04-09 15:23:42 +0200

    Assemble the jar release

--- .jenkins/internalRelease.groovy
+++ .jenkins/internalRelease.groovy
@@ -8,6 +8,13 @@ agent('kotlin') {
         gitCheckout()
     }
 
+    stage('Assemble the jar') {
+        android.gradle "assemble"
+        // Finally upload the files
+        String releaseId = ghUtils.getReleaseId(tag)
+        ghUtils.uploadReleaseAssets(releaseId, "build/libs/net.grandcentrix.either-1.0-SNAPSHOT.jar")
+    }
+
     stage('Publish to artifactory') {
         withCredentials([string(credentialsId: "artifactory-deployment", variable: 'API_KEY')]) {
             android.gradle "artifactoryPublish -PpublishToInternal -PartifactoryUser=jenkins -PartifactoryKey=${API_KEY}"
