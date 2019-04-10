5b89bd07387002dd5ebfc4e584ddc2605c7880f6
obogdanov <oleksandr.bogdanov@grandcentrix.net>
2019-04-10 12:07:54 +0200

    remove either value tests

--- src/test/kotlin/net/grandcentrix/either/EitherTest.kt
+++ src/test/kotlin/net/grandcentrix/either/EitherTest.kt
@@ -2,29 +2,8 @@ package net.grandcentrix.either
 
 import org.junit.jupiter.api.Test
 
+
 class EitherTest {
 
-    @Test
-    fun Failure() {
-        val either: Either<Int, Int> = Failure(123)
 
-        val result = when (either) {
-            is Failure  -> either.failure * 2
-            is Success -> -1
-        }
-
-        check(result == 123 * 2)
-    }
-
-    @Test
-    fun Success() {
-        val either: Either<Int, String> = Success("Success")
-
-        val result = when (either) {
-            is Failure  -> "${either.failure}Failure"
-            is Success -> "${either.success}Success"
-        }
-
-        check(result == "SuccessSuccess")
-    }
 }
