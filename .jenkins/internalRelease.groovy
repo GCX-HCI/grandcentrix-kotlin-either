fb9849b419caace959ab8c9d47ec8ae4766681da
obogdanov <oleksandr.bogdanov@grandcentrix.net>
2019-04-10 10:26:52 +0200

    add stage('Create GH Release')

--- .jenkins/internalRelease.groovy
+++ .jenkins/internalRelease.groovy
@@ -12,6 +12,25 @@ agent('linux') {
         android.gradle "assemble"
     }
 
+    GitHubUtils ghUtils = new GitHubUtils()
+    stage('Create GH Release') {
+        String tag = "${TAG}"
+        String branch = "${BRANCH}"
+
+        // Create the git TAG
+        ghUtils.createAndUploadTag(tag)
+
+        // Then we create the release (based on the tag)
+        ghUtils.createRelease([
+                "tag"         : tag,
+                "tagBranch"   : branch,
+                "title"       : tag,
+                "releaseNotes": tag,
+                "asDraft"     : false,
+                "asPrerelease": true
+        ])
+    }
+
     stage('Publish to artifactory') {
         withCredentials([string(credentialsId: "artifactory-deployment", variable: 'API_KEY')]) {
             android.gradle "artifactoryPublish -PpublishToInternal -PartifactoryUser=jenkins -PartifactoryKey=${API_KEY}"
