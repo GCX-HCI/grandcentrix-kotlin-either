1ee414bc488184f894523bbedd1f7c50d60f5f95
obogdanov <oleksandr.bogdanov@grandcentrix.net>
2019-04-10 12:08:56 +0200

    test flat map

--- src/test/kotlin/net/grandcentrix/either/EitherTest.kt
+++ src/test/kotlin/net/grandcentrix/either/EitherTest.kt
@@ -5,5 +5,14 @@ import org.junit.jupiter.api.Test
 
 class EitherTest {
 
+    @Test
+    fun `test flatMap returns mapped value`() {
+        val eitherOriginal: Either<Int, String> = Success("original result")
+        val eitherMapTo: Either<Int, String> = Success("map to result")
 
+        val mappedEither = eitherOriginal.flatMap { eitherMapTo }
+
+        val result = mappedEither as Success
+        check(result.success == "map to result")
+    }
 }
