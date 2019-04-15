b8fa5d909893ff1d8f7328d6b124cdca75686efb
Stefan May <stefan.may@grandcentrix.net>
2019-04-15 12:49:43 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ apply(plugin = "org.jetbrains.kotlin.jvm")
 apply(plugin = "guru.stefma.artifactorypublish")
 
 group = "net.grandcentrix.either"
-version = "1.1"
+version = "1.2"
 
 repositories {
     mavenCentral()
