b8d48e277a483a8350dc2ef76f593254b6de074d
StefMa <Stefan.May@grandcentrix.net>
2019-04-15 10:48:29 +0200

    Use JUnit Platform to recognize the tests by gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.12.2")
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 configure<ArtifactsExtension> {
     artifactId = "either"
 }
