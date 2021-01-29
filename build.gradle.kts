07e6db6b328be6c5a612629006499c144705ce5d
Thomas Weingardt <thomas.weingardt@grandcentrix.net>
2021-01-29 15:53:31 +0100

    remove old publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import guru.stefma.androidartifacts.ArtifactsExtension
-import guru.stefma.artifactorypublish.ArtifactoryPublishExtension
-
 buildscript {
     repositories {
         google()
@@ -9,12 +6,10 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
-        classpath("guru.stefma.artifactorypublish:artifactorypublish:1.2.0")
     }
 }
 
 apply(plugin = "org.jetbrains.kotlin.jvm")
-apply(plugin = "guru.stefma.artifactorypublish")
 
 group = "net.grandcentrix.either"
 version = "1.3"
@@ -36,21 +31,4 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-configure<ArtifactsExtension> {
-    artifactId = "either"
-}
-
-configure<ArtifactoryPublishExtension> {
-    artifactoryUrl = when {
-	hasProperty("publishToPublic") -> "https://repo.gcxi.de"
-	else 			       -> "https://artifactory.gcxi.de"
-    }
-    artifactoryRepo = when {
-	hasProperty("publishToInternal") -> "maven-internal" 
-	hasProperty("publishToPublic") 	 -> "maven"
-	else 				 -> "maven-playground"
-    }
-    publications = arrayOf("maven")
 }
