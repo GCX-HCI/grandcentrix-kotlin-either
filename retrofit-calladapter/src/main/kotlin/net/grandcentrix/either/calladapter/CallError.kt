ff875be8d71de21315d32fdedf420b2f9cb7952d
Jan Reehuis <jan.reehuis@grandcentrix.net>
2021-02-19 13:31:11 +0100

    improves the CallError cause throwable declarations

--- retrofit-calladapter/src/main/kotlin/net/grandcentrix/either/calladapter/CallError.kt
+++ retrofit-calladapter/src/main/kotlin/net/grandcentrix/either/calladapter/CallError.kt
@@ -3,28 +3,27 @@ package net.grandcentrix.either.calladapter
 /**
  * Error hierarchy when calling remote server.
  */
-sealed class CallError(open val throwable: Throwable? = null)
+sealed class CallError(open val cause: Throwable?)
 
 /**
  * Http request returned an error response.
  */
-data class HttpError(val code: Int, val body: String) : CallError(Exception(body))
+data class HttpError(val code: Int, val body: String, override val cause: Throwable? = null) : CallError(cause)
 
 /**
  * The request timed out.
  */
-data class TimeoutError(override val throwable: Throwable) : CallError(throwable)
+data class TimeoutError(override val cause: Throwable? = null) : CallError(cause)
 
 /**
  * Network error on client.
  */
-data class NetworkError(override val throwable: Throwable) : CallError(throwable)
+data class NetworkError(override val cause: Throwable) : CallError(cause)
 
 /**
  * Unknown API error.
  */
-data class UnexpectedCallError(override val throwable: Throwable = Exception("Unknown Call error")) :
-    CallError(throwable) {
+data class UnexpectedCallError(override val cause: Throwable) : CallError(cause) {
 
     constructor(message: String) : this(Exception(message))
 }
